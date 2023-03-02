acf42fadf9641b397cdded193b2bc2abeca78779
7Ajithra7 <126359618+7Ajithra7@users.noreply.github.com>
2023-03-02 09:17:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ Mongoose.connect("mongodb+srv://ajithra:ajithra@cluster000.hjf3qzh.mongodb.net/C
 
 
 const path = require('path');
-app.use(express.static(path.join(__dirname,'/build')));
+app.use(Express.static(path.join(__dirname,'/build')));
 app.get("/*", function(req, res) {
 res.sendFile(path.join(__dirname,'/build/index.html')); }); 
 
