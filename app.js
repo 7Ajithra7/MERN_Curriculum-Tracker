21f91a247de2966653bc22cead90c49196167f3b
7Ajithra7 <126359618+7Ajithra7@users.noreply.github.com>
2023-03-02 09:14:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,98 +28,10 @@ Mongoose.connect("mongodb+srv://ajithra:ajithra@cluster000.hjf3qzh.mongodb.net/C
 })
 
 
-// test api
-// app.use('/',AppRouter)
-
-
-
-//DashBoard Api
-//Admin Module:
-//Faculty Module:
-//Apis
-
-
-// router.get('/', function (req, res, next) {
-// 	return res.render('index.ejs');
-// });
-
-
-// app.post('/', function(req, res, next) {
-// 	console.log(req.body);
-// 	var personInfo = req.body;
-
-
-// 	if(!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf){
-// 		res.send();
-// 	} else {
-// 		if (personInfo.password == personInfo.passwordConf) {
-
-// 			User.findOne({email:personInfo.email},function(err,data){
-// 				if(!data){
-// 					var c;
-// 					User.findOne({},function(err,data){
-
-// 						if (data) {
-// 							console.log("if");
-// 							c = data.unique_id + 1;
-// 						}else{
-// 							c=1;
-// 						}
-
-// 						var newPerson = new User({
-// 							unique_id:c,
-// 							email:personInfo.email,
-// 							username: personInfo.username,
-// 							password: personInfo.password,
-// 							passwordConf: personInfo.passwordConf
-// 						});
-
-// 						newPerson.save(function(err, Person){
-// 							if(err)
-// 								console.log(err);
-// 							else
-// 								console.log('Success');
-// 						});
-
-// 					}).sort({_id: -1}).limit(1);
-// 					res.send({"Success":"You are regestered,You can login now."});
-// 				}else{
-// 					res.send({"Success":"Email is already used."});
-// 				}
-
-// 			});
-// 		}else{
-// 			res.send({"Success":"password is not matched"});
-// 		}
-// 	}
-// });
-
-// app.get('/login', function (req, res, next) {
-// 	return res.render('login.ejs');
-// });
-
-// app.post('/login', function (req, res, next) {
-// 	//console.log(req.body);
-// 	User.findOne({email:req.body.email},function(err,data){
-// 		if(data){
-			
-// 			if(data.password==req.body.password){
-// 				//console.log("Done Login");
-// 				req.session.userId = data.unique_id;
-// 				//console.log(req.session.userId);
-// 				res.send({"Success":"Success!"});
-				
-// 			}else{
-// 				res.send({"Success":"Wrong password!"});
-// 			}
-// 		}else{
-// 			res.send({"Success":"This Email Is not regestered!"});
-// 		}
-// 	});
-// });
-
-
-
+const path = require('path');
+app.use(express.static(path.join(__dirname,'/build')));
+app.get("/*", function(req, res) {
+res.sendFile(path.join(__dirname,'/build/index.html')); }); 
 
 
 
